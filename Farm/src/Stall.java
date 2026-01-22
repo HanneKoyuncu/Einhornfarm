953a3e5c86ff82ccdb0cc591a70e123b284a0471
HanneKoyuncu <hm.koyuncu22@gmail.com>
2026-01-22 11:38:47 +0100

    Update Stall.java

--- Farm/src/Stall.java
+++ Farm/src/Stall.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stall {
+
+    private int maxCapacity;
+    private List<Einhorn> einhorner;
+
+    public Stall(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        this.einhorner = new ArrayList<>();
+    }
+
+    public boolean addEinhorn(Einhorn e) {
+        if (einhorner.size() >= maxCapacity) {
+            return false;
+        }
+        einhorner.add(e);
+        return true;
+    }
+
+    public void removeEinhorn(Einhorn e) {
+        einhorner.remove(e); // Spieler wählt selbst
+    }
+
+    public List<Einhorn> getEinhorner() {
+        return einhorner;
+    }
+
+    public boolean isFull() {
+        return einhorner.size() >= maxCapacity;
+    }
 }
