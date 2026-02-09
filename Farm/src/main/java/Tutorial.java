50f85d8512870ad4c590337ea93e066661e1aa1b
HanneKoyuncu <hm.koyuncu22@gmail.com>
2026-02-09 20:31:12 +0100

    Update Tutorial.java

--- Farm/src/main/java/Tutorial.java
+++ Farm/src/main/java/Tutorial.java
@@ -1,10 +1,23 @@
 import abitur.List;
 
+/**
+ * Die Klasse Tutorial verwaltet eine Schritt-für-Schritt-Anleitung
+ * für den Spieler. Die Schritte werden nacheinander ausgegeben,
+ * z. B. als Hilfetext im Spiel.
+ */
 public class Tutorial {
 
+    /** Liste aller Tutorial-Schritte */
     private List<String> schritte;
+
+    /** Aktueller Index des Tutorials */
     private int index;
 
+    /**
+     * Konstruktor des Tutorials.
+     * Initialisiert die Liste der Schritte und fügt alle
+     * Tutorial-Texte hinzu.
+     */
     public Tutorial() {
         schritte = new List<>();
         index = 0;
@@ -17,22 +30,43 @@ public class Tutorial {
         schritte.append("Du kannst ausgewählte Einhörner auch verkaufen oder bewegen.");
         schritte.append("Viel Spaß beim Spielen!");
     }
-//nächster Schritt wird aufgerufen
+
+    /**
+     * Gibt den nächsten Tutorial-Schritt zurück.
+     *
+     * @return Der nächste Tutorial-Text oder null,
+     *         wenn keine Schritte mehr vorhanden sind.
+     */
     public String naechsterSchritt() {
         if (index < anzahl()) {
             schritte.toFirst();
-            for (int i = 0; i < index; i++) schritte.next();
+
+            // Zum aktuellen Schritt springen
+            for (int i = 0; i < index; i++) {
+                schritte.next();
+            }
+
             String s = schritte.getContent();
             index++;
             return s;
         }
         return null;
     }
-//prüft ob mehr Schritte vorhanden sind
+
+    /**
+     * Prüft, ob noch weitere Tutorial-Schritte vorhanden sind.
+     *
+     * @return true, wenn noch Schritte vorhanden sind, sonst false
+     */
     public boolean hatMehrSchritte() {
         return index < anzahl();
     }
-//Anzahl der Schritte wird zurückgegeben
+
+    /**
+     * Zählt die Anzahl der Tutorial-Schritte.
+     *
+     * @return Anzahl der gespeicherten Schritte
+     */
     private int anzahl() {
         int count = 0;
         schritte.toFirst();
@@ -43,3 +77,4 @@ public class Tutorial {
         return count;
     }
 }
+
