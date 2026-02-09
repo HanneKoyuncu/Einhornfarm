2a50486e3c3992c49992b206578b4082bfab54ac
HanneKoyuncu <hm.koyuncu22@gmail.com>
2026-02-09 20:07:10 +0100

    Update Einhorn.java

--- Farm/src/main/java/Einhorn.java
+++ Farm/src/main/java/Einhorn.java
@@ -1,17 +1,49 @@
+/**
+ * Die Klasse Einhorn repräsentiert ein einzelnes Einhorn im Spiel.
+ * Jedes Einhorn besitzt ein Attribut (z.B. FEUER, WASSER, LAVA)
+ * sowie eine dazugehörige Seltenheit.
+ *
+ * Die Seltenheit wird automatisch aus dem Attribut abgeleitet
+ * und bestimmt z.B. den Verkaufswert des Einhorns.
+ */
 public class Einhorn {
 
+    /**
+     * Das Attribut des Einhorns, welches seine Eigenschaften festlegt.
+     */
     private Attribute attribut;
+
+    /**
+     * Die Seltenheit des Einhorns, abhängig vom Attribut.
+     */
     private Seltenheit seltenheit;
 
+    /**
+     * Konstruktor für ein Einhorn.
+     * Beim Erstellen eines Einhorns wird dessen Seltenheit
+     * automatisch aus dem Attribut bestimmt.
+     *
+     * @param attribut das Attribut des Einhorns
+     */
     public Einhorn(Attribute attribut) {
         this.attribut = attribut;
         this.seltenheit = attribut.getSeltenheit();
     }
 
+    /**
+     * Gibt das Attribut des Einhorns zurück.
+     *
+     * @return Attribut des Einhorns
+     */
     public Attribute getAttribut() {
         return attribut;
     }
 
+    /**
+     * Gibt die Seltenheit des Einhorns zurück.
+     *
+     * @return Seltenheit des Einhorns
+     */
     public Seltenheit getSeltenheit() {
         return seltenheit;
     }
