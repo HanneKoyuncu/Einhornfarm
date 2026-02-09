28f1724174768984863da262673bce057037be49
HanneKoyuncu <hm.koyuncu22@gmail.com>
2026-02-09 20:26:09 +0100

    Update Seltenheit.java

--- Farm/src/main/java/Seltenheit.java
+++ Farm/src/main/java/Seltenheit.java
@@ -1,15 +1,42 @@
+/**
+ * Die Enum Seltenheit beschreibt, wie selten ein Einhorn ist.
+ * Jede Seltenheit besitzt einen festen Wert, der z.B.
+ * beim Verkauf eines Einhorns als Coin-Betrag verwendet wird.
+ */
 public enum Seltenheit {
+
+    // Häufiges Einhorn, niedriger Verkaufswert
     GEWÖHNLICH(10),
+
+    // Seltener als gewöhnlich
     SELTEN(25),
+
+    // Sehr selten
     EPISCH(50),
+
+    // Extrem selten, hoher Wert
     LEGENDÄR(100);
 
+    /**
+     * Der Coin-Wert dieser Seltenheit
+     */
     private int wert;
 
+    /**
+     * Konstruktor der Enum.
+     * Jeder Seltenheit wird ein fester Wert zugewiesen.
+     *
+     * @param wert Coin-Wert beim Verkauf
+     */
     Seltenheit(int wert) {
         this.wert = wert;
     }
 
+    /**
+     * Gibt den Coin-Wert der Seltenheit zurück.
+     *
+     * @return Coin-Wert
+     */
     public int getWert() {
         return wert;
     }
